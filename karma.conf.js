1e48bcb7700c7831a390b1a0104329082db6f886
Nico31300 <nicolas.couret@gmail.com>
2020-08-21 15:17:11 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,9 @@
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-module.exports = function(config) {
-  "use strict";
+module.exports = function (config) {
   config.set({
-
-    frameworks: ['ui5'],
-    reporters: ["progress"],
-
-    browsers: ["Chrome_without_security"],
-    
-    customLaunchers: {
-      Chrome_without_security: {
-         base: 'ChromeHeadless',
-         flags: ['--disable-web-security', '--no-sandbox']
-      }
-    },
-    singleRun: true
-
-  });
-};
+    frameworks: ['qunit'],
+    plugins: ['karma-qunit'],
+    files: [
+      '*.js'
+    ]
+  })
+}
