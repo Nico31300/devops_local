51f3c6f8c52552f7d8817db727207987496e783e
Nico31300 <nicolas.couret@gmail.com>
2020-08-21 16:45:03 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
     plugins: ['karma-qunit','karma-junit-reporter','karma-chrome-launcher', 'karma-webdriver-launcher'],
     
     files: [
-      './webapp/test/testsuite.qunit.js'
+      './webapp/test/testsuite.qunit.html'
     ],
     
     // level of logging
