9863ba7e845fec9f9d1fbebf31e215ae84d9a872
Nico31300 <nicolas.couret@gmail.com>
2020-08-21 16:05:08 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,39 @@
 module.exports = function (config) {
+  
+  var webdriverConfig = {
+    hostname: 'selenium',
+    port: 4444
+  }
+  
   config.set({
     frameworks: ['qunit'],
-    plugins: ['karma-qunit'],
+    plugins: ['karma-qunit','karma-junit-reporter','karma-chrome-launcher', 'karma-webdriver-launcher'],
+    
     files: [
       './webapp/test/testsuite.qunit.js'
-    ]
+    ],
+    
+    reporters: ['junit'],
+    
+    customLaunchers: {
+        'chromeSel': {
+            base: 'WebDriver',
+            config: webdriverConfig,
+            browserName: 'chrome'
+        }
+    },
+    
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['chromeSel'],
+    
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+    junitReporter: {
+        outputFile: 'test-results-karma.xml',
+        suite: ''
+    }
+    
   })
 }
