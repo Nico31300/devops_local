e4dc77523ce9f3727239728465671a08661def4c
Nico31300 <nicolas.couret@gmail.com>
2020-08-21 17:19:59 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
   }
   
   config.set({
-    frameworks: ['qunit','ui5'],
+    frameworks: ['ui5'],
     plugins: ['karma-qunit','karma-junit-reporter','karma-chrome-launcher', 'karma-webdriver-launcher','karma-ui5'],
     
     ui5: {
