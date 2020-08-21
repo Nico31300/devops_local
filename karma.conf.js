9784c5060128ac29177a3948bbbf9587f4b976d0
Nico31300 <nicolas.couret@gmail.com>
2020-08-21 17:00:13 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -6,8 +6,12 @@ module.exports = function (config) {
   }
   
   config.set({
-    frameworks: ['qunit'],
+    frameworks: ['qunit','ui5'],
     plugins: ['karma-qunit','karma-junit-reporter','karma-chrome-launcher', 'karma-webdriver-launcher'],
+    
+    ui5: {
+      url: "https://sapui5.hana.ondemand.com/resources/sap-ui-core.js"
+    },
     
     files: [
       'webapp/test/unit/AllTests.js'
