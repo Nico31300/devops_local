656050dfcd6c21fc3c4744dee5858fc43413348f
Nico31300 <nicolas.couret@gmail.com>
2020-08-24 13:50:14 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,12 @@
 module.exports = function (config) {
   
-  var webdriverConfig = {
-    hostname: 'selenium',
-    port: 4444
-  }
-  
   config.set({
     frameworks: ['ui5'],
-    //plugins: ['karma-qunit','karma-junit-reporter','karma-chrome-launcher', 'karma-webdriver-launcher','karma-ui5'],
     
     ui5: {
       url: "https://sapui5.hana.ondemand.com"
     },
-    /*
-    files: [
-      'webapp/test/unit/AllTests.js'
-    ],*/
-    
+        
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_DEBUG,
@@ -24,16 +14,13 @@ module.exports = function (config) {
     customLaunchers: {
         'chromeSel': {
             base: 'WebDriver',
-            config: webdriverConfig,
+            config: { hostname: 'selenium', port: 4444 },
             browserName: 'chrome'
         }
     },
     
     hostname: 'karma',
     port: 9876,
-    
-
-    //reporters: ['junit'],
     
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -42,7 +29,9 @@ module.exports = function (config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
+        
     /*,
+    reporters: ['junit'],
     junitReporter: {
         outputFile: 'test-results-karma.xml',
         suite: ''
