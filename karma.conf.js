48f88f0ebc6141fbbeac1183a4d66c169b42b1ba
Nico31300 <nicolas.couret@gmail.com>
2020-08-26 15:10:58 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,13 @@
-module.exports = function (config) {
-  
+module.exports = function(config) {
   config.set({
-    frameworks: ['ui5'],    
-    //UI5 framework
+
+    frameworks: ["ui5"],
+
     ui5: {
       url: "https://openui5.hana.ondemand.com"
     },
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-    
-    customLaunchers: {
-        'chromeSel': {
-            base: 'WebDriver',
-            config: { hostname: 'selenium', port: 4444 },
-            browserName: 'chrome'
-        }
-    },
-    
-    hostname: 'karma',
-    port: 9876,
-    
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['chromeSel'],
-    
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-        
-    /*,
-    reporters: ['junit'],
-    junitReporter: {
-        outputFile: 'test-results-karma.xml',
-        suite: ''
-    }*/
-    
-  })
-}
+
+    browsers: ["Chrome"]
+
+  });
+};
